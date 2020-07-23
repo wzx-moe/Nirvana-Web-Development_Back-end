7cee653b827d7b6e31b6102978fb480224df5e59
Stephen Evenson <1258455347@qq.com>
2020-07-24 01:51:54 +0800

    添加get属性

--- src/main/java/com/wzx/wzx_test1/User.java
+++ src/main/java/com/wzx/wzx_test1/User.java
@@ -2,13 +2,24 @@ package com.wzx.wzx_test1;
 
 public class User {
     private String UserName;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public String getPassWord() {
+        return PassWord;
+    }
+
     private String PassWord;
 
+
+
     public void setUserName( String userName ){
-        UserName = userName;
+        this.UserName = userName;
     }
 
     public void setPassWord( String passWord ){
-        PassWord = passWord;
+        this.PassWord = passWord;
     }
 }
