9a69fc8db283feeac2c0b7489dd2b7501a4911ed
WZX <wzx00051111@gmail.com>
2020-07-23 18:20:22 +0800

    initial project version

---
+++ src/main/java/com/wzx/wzx_test1/WzxTest1Application.java
@@ -0,0 +1,13 @@
+package com.wzx.wzx_test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WzxTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WzxTest1Application.class, args);
+    }
+
+}
