afd89d0a4db565716cdb37493f5985aff16bbd6e
Stephen Evenson <1258455347@qq.com>
2020-07-24 21:31:33 +0800

    添加获取验证码图片的数据库操作

---
+++ src/main/java/com/wzx/wzx_test1/mapper/SignMapper.java
@@ -0,0 +1,21 @@
+package com.wzx.wzx_test1.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface SignMapper {
+    @Select("SELECT `sessionId` FROM `verificationCode` WHERE `sessionId`=#{sessionId}")
+    String getSessionId(String sessionId);
+
+    @Insert("INSERT into `verificationCode` (`verificationCode`, `sessionId`) VALUE (#{verCode}, #{sessionId})")
+    String addVerCode(String verCode, String sessionId);
+
+    @Update("UPDATE `verificationCode` SET `verificationCode`=#{verCode} WHERE sessionId=#{sessionId}")
+    int updateVerCode(String verCode, String sessionId);
+
+    @Update("UPDATE `userDtl` SET sessionId=null WHERE sessionId=#{sessionId}")
+    int deleteSessionId(String sessionId);
+}
